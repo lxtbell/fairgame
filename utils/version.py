35b3f2b746438fc63548767940b492c4ddd55cd9
Xiaotian Le <lxtbell@gmail.com>
2021-04-02 00:46:52 -0700

    Fix KeyError: 'tag_name'

--- utils/version.py
+++ utils/version.py
@@ -59,7 +59,7 @@ def get_latest_version():
             return latest_version
         data = r.json()
         latest_version = parse(str(data["tag_name"]))
-    except InvalidVersion:
+    except Exception:
         # Return a safe, but wrong version
         latest_version = parse("0.0")
     return latest_version
